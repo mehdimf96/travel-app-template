4cde4530e75660dc74fc630c92ce8a398a11fb43
mehdimf96 <mehranfar.7575@gmail.com>
2025-08-19 14:24:04 +0330

    Initial app

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View className="">
+      <Text className="text-blue-800 font-bold text-3xl mt-2">
+        Edit app/index.jsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
